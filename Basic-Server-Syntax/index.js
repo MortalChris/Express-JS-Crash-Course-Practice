c3ded6088f1f6d80f9c5708c5630796065116a6d
Christian Valeri <mortlaxris@outlook.com>
2023-10-11 22:18:29 -0500

    Add static fol, styles, another html in public

--- Basic-Server-Syntax/index.js
+++ Basic-Server-Syntax/index.js
@@ -5,10 +5,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // //Create your endpoints/route handles
-app.get('/', (req, res) => {
-    // res.send('<h1>Hello World!</h1>')
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));//public is the name of the foler, index.html is the file within
-});
+// app.get('/', (req, res) => {
+//     // res.send('<h1>Hello World!</h1>')
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));//public is the name of the foler, index.html is the file within
+// });
+
+//Set Static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // //Listen on Port
 app.listen(port, () => {
