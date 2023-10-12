d0211c6859d35f45eab05e335a8d0d36372db48a
Christian Valeri <mortlaxris@outlook.com>
2023-10-11 21:55:46 -0500

    Checks if there is a seperate port if not uses def

--- Basic-Server-Syntax/index.js
+++ Basic-Server-Syntax/index.js
@@ -1,14 +1,14 @@
-const express = require('express')
+const express = require('express');
 // //Init express
-const app = express()
-const port = 3000
+const app = express();
+const port = processs.env.PORT || 3000;
 
 // //Create your endpoints/route handles
 app.get('/', (req, res) => {
-res.send('Hello World!')
-})
+    res.send('Hello World!')
+});
 
 // //Listen on Port
 app.listen(port, () => {
     console.log(`Website on http://localhost:${port}`)
-})
+});
